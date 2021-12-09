f4092d7b731e7b58660fcbb9b6c92e0e367fa289
yangjc <binqian12@163.com>
2021-12-09 16:14:36 +0800

    update

---
+++ leetcode-study/src/test/java/com/study/example/leetcodestudy/LeetcodeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.example.leetcodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
